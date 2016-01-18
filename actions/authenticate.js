c24ecf83b1deb938681bb5d7d9ec05be9e9127b8
Ruben Gerits <ruben.gerits@onprvp.fgov.be>
2016-01-18 12:49:47 +0100

    Delete github token

--- actions/authenticate.js
+++ actions/authenticate.js
@@ -6,10 +6,11 @@ var authenticate = {};
 authenticate.execute = function(authorization, callback) {
 	github.createAuth(authorization, function(result) {
 		travisci.createAuth(result.token, function(result) {
-			github.deleteAuth(result.id, authorization);
-
-			callback({
-				'accessToken' : result.access_token
+			var token = result.access_token;
+			github.deleteAuth(result.id, authorization, function() {
+				callback({
+					'accessToken' : token
+				});
 			});
 		});
 	});
